1e0c02802d04d9669b3727bdd8ce808f3f38d6e5
Zubair12345678910 <zubairsheihh67@gmail.com>
2023-10-13 09:12:17 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+
+
+
+//  -----1st-------
+
+var username1 = "zubair"
+var username2 = "sheikh"
+
+alert(username1 +" "+ username2)
+
+// ------2nd-----
+
+var age = "22 years old"
+alert(age)
+
+// ------3rd------
+
+var education = "certified web & app development"
+alert(education)
+
+// -------4th-------
+
+alert("Pizza \n Pizz \n Piz \n Pi \n  P \n")
+
+// ------5th-----
+
+var email = "zubairsheikh0241@gmail.com"
+alert(email)
+
+// -------6th-----
+
+var Javascript = "I am trying to learn form the Book A smarter way to learn JavaScript"
+alert(Javascript)
+
+// -------7th-----
+
+var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(design)
